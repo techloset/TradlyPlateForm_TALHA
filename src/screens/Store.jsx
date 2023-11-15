7c470e64791c4cda91d027a22ac456e93932a380
talhaunas1 <mtalha4600@gmail.com>
2023-11-16 00:15:10 +0500

    working on Store Screen

--- src/screens/Store.jsx
+++ src/screens/Store.jsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import ratio from '../styles/ratio';
+const {widthPixel, fontPixel, pixelSizeVertical} = ratio;
+import Back from '../assets/images/Back.svg';
+import {COLOR, FONT_FAMILY} from '../styles/GlobalStyles';
+import CartIcon from '../assets/images/cartIcon.svg';
+import Whishlist from '../assets/images/wishListIcon.svg';
 
 const Store = () => {
   return (
-    <View>
-      <Text>Store</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles.main}>
+        <View style={styles.backContainer}>
+          <Text style={styles.textCart}>My Store </Text>
+        </View>
+        <View style={styles.ImageContainer}>
+          <View style={styles.CartIcon}>
+            <Whishlist />
+          </View>
+          <View >
+            <CartIcon />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Store
+export default Store;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: COLOR.bodygreen,
+    height: widthPixel(116),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:"space-between",
+    // marginHorizontal: pixelSizeVertical(16),
+  },
+  backContainer: {
+    marginLeft: pixelSizeVertical(16),
+    justifyContent: 'center',
+  },
+  ImageContainer: {
+    flexDirection: 'row',
+    marginRight: pixelSizeVertical(16),
+  },
+  textCart: {
+    fontSize: fontPixel(24),
+    fontFamily: FONT_FAMILY.Montserrat,
+    fontWeight: '700',
+    color: COLOR.white,
+  },
+  CartIcon:{
+    marginRight: pixelSizeVertical(16),
+  }
+});
